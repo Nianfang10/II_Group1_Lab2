269f8fc437945ccd5382f2e0cd726fb1135b21fa
Nianfang Shi <91589561+Nianfang10@users.noreply.github.com>
2021-11-09 18:36:12 +0100

    return winsize*winsize*channels

--- glcm.py
+++ glcm.py
@@ -5,8 +5,8 @@ import cv2
 import numpy as np
 
 
-def getGLCM(data_win):
-    imgBGR = np.flip(data_win[:, :, 0:3], -1)  # convert the channels into B, G, R
+def getGLCM(input):
+    imgBGR = np.flip(input[:, :, 0:3], -1)  # convert the channels into B, G, R
     imgGray = cv2.cvtColor(np.float32(imgBGR), cv2.COLOR_BGR2GRAY)
     imgGray_int = imgGray.astype('int')  # the greycomatrix function below does not allow "float" type
 
@@ -20,7 +20,7 @@ def getGLCM(data_win):
     h = glcm_h[:, :, 0, 0]
     v = glcm_v[:, :, 0, 0]
     diag = glcm_45[:, :, 0, 0]
-    joint = np.array([h, v, diag])  # return a 3*winsize*winsize matrix
+    joint = np.array([h, v, diag]).transpose((1, 2, 0))  # return a winsize*winsize*3 matrix
     return joint
 
 
