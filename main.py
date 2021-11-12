231f3648b98cbc95731d11d340493a9d2d8c66c6
Nianfang Shi <91589561+Nianfang10@users.noreply.github.com>
2021-11-12 18:16:25 +0100

    combined features, cannot run yet

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import h5py
 import numpy as np
 from tqdm import tqdm
 
+
 from sklearn.preprocessing import StandardScaler
 
 
@@ -18,10 +19,16 @@ train_loader = torch.utils.data.DataLoader(dset,
 
 
 # define the regressors
-from sklearn.linear_model import SGDRegressor, PassiveAggressiveRegressor 
-import xgboost as xgb
+# from sklearn.linear_model import SGDRegressor, PassiveAggressiveRegressor
+from sklearn.preprocessing import scale
 
-SGD_6feat = SGDRegressor()
+# import xgboost as xgb
+from sklearn.linear_model import SGDRegressor
+from sklearn.pipeline import make_pipeline
+# build a linear model fitted by minimizing a regularized empirical loss with SGD
+reg = SGDRegressor(max_iter=1000, tol=1e-3)
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
 
 
 for X,Y in tqdm(train_loader):
@@ -32,17 +39,45 @@ for X,Y in tqdm(train_loader):
         
         x = np.asarray(X[i])
         y = Y[i]
-        print(x.shape)
         # print(x.shape,y.shape)
-        ARVI = np.expand_dims(HCF.getARVI(x), axis=-1)
+        R = np.expand_dims(x[:, :, 0], axis=-1)
+        G = np.expand_dims(x[:, :, 1], axis=-1)
+        B = np.expand_dims(x[:, :, 2], axis=-1)
+        NIR = np.expand_dims(x[:, :, 3], axis=-1)
         NDVI = np.expand_dims(HCF.getNDVI(x), axis=-1)
-        
-        # x = np.dstack(x,hsv)
-        # x = np.dstack(x,lab)
-        sobel =  np.expand_dims(HCF.HSV(x), axis=-1)
-        # print(sobel.shape)
-        # x = np.concatenate([x, ARVI, sobel], axis=-1)
-        # print(x.shape)
+        MSAVI = np.expand_dims(HCF.getMSAVI(x), axis=-1)
+        VARI = np.expand_dims(HCF.getVARI(x), axis=-1)
+        ARVI = np.expand_dims(HCF.getARVI(x), axis=-1)
+        GCI = np.expand_dims(HCF.getGCI(x), axis=-1)
+        SIPI = np.expand_dims(HCF.getSIPI(x), axis=-1)
+
+        HSV = HCF.HSV(x)
+        LAB = HCF.LAB(x)
+        SOBEL = HCF.SOBEL(x)
+        # PREWITT = np.expand_dims(HCF.PREWITT(x), axis=-1)
+        # print(PREWITT.shape)
+        LBP = HCF.LBP(x)
+        # GLCM = HCF.getGLCM(x)
+
+        # hcf_list = [R, G, B, NIR, NDVI, MSAVI, VARI, ARVI, GCI, SIPI,
+        #             HSV, LAB, SOBEL, PREWITT, LBP]
+        hcf_list = [R, G, B, NIR, NDVI, MSAVI, VARI, ARVI, GCI, SIPI,
+                    HSV, LAB, SOBEL, LBP]
+        all_features = np.concatenate(hcf_list, axis=2)
+        # print(all_features.shape)
+
+        all_feat_2D = np.reshape(all_features, (-1, all_features.shape[-1]))
+        scaler_x.partial_fit(all_feat_2D)
+        x_std = scaler_x.transform(all_features)
+        scaler_y.partial_fit(y)
+        ground_truth = scaler.transform(y)
+        reg.partial_fit(all_features, ground_truth)
+
+
+
+# reg_pre = reg.predict()
+
+
 
 
 
